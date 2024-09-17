199cafb3d407753ec462ec65ed8c17457a03b311
Vinayak <vinayak.kalmane@codecraft.co.in>
2024-09-17 09:41:47 +0530

    Corrected middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,7 @@ export async function middleware(req: NextRequest) {
 
   const token = await getToken({
     req,
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXT_AUTH_SECRET,
     secureCookie: process.env.NODE_ENV === "production",
   });
 
