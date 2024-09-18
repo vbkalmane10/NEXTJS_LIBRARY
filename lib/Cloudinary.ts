01464f8ce5bd6beb1b4f88e0ff8f2edd36cace58
Vinayak <vinayak.kalmane@codecraft.co.in>
2024-09-18 13:01:06 +0530

    Removed part

--- lib/Cloudinary.ts
+++ lib/Cloudinary.ts
@@ -1,21 +1,18 @@
-import { v2 as cloudinary } from "cloudinary";
-
-
-cloudinary.config({
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
 export const uploadImageToCloudinary = async (file: File): Promise<string> => {
   const formData = new FormData();
   formData.append("file", file);
-  formData.append("upload_preset", process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET || "default");
+  formData.append(
+    "upload_preset",
+    process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET || "default"
+  );
 
-  const response = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`, {
-    method: "POST",
-    body: formData,
-  });
+  const response = await fetch(
+    `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
 
   const data = await response.json();
 
@@ -23,5 +20,5 @@ export const uploadImageToCloudinary = async (file: File): Promise<string> => {
     throw new Error(data.error.message);
   }
 
-  return data.secure_url; // Cloudinary URL of the uploaded image
+  return data.secure_url;
 };
